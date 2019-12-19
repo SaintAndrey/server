47406644c7e4f3e2f580577e97e2fd86d9d45b75
Andrey Svetlov <andrersv96@gmail.com>
2019-12-19 13:34:14 +0300

    Some change

--- index.js
+++ index.js
@@ -7,5 +7,16 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send('ты хуй'))
+  .get('/cool', (req, res) => {
+	request({
+		headers: {
+			'Device-Id': 'curl',
+			'Device-OS': 'linux'
+		},
+		uri: 'https://+79117875764:681824@proverkacheka.nalog.ru:9999/v1/inns/*/kkts/*/fss/9284000100198625/tickets/79725?fiscalSign=1336674326&sendToEmail=no',
+		method: 'GET'
+	}, function (err, res, body) {
+		res.send(body)
+	})
+})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
